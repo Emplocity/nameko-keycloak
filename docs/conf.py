64a2ad0f1b9fc17a2932edaf3d9192f89d0d33d4
Zbigniew Siciarz <zbigniew@siciarz.net>
2022-01-11 09:36:11 +0100

    Bump version: 0.2.0 → 0.3.0

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ project = "nameko-keycloak"
 year = "2021"
 author = "Emplocity"
 copyright = "{0}, {1}".format(year, author)
-version = release = "0.2.0"
+version = release = "0.3.0"
 
 pygments_style = "trac"
 templates_path = ["."]
